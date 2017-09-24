22af3252913e8df51ffc0976530a83e3f052428c
Evgeny Shlykov <e.a.shlykov@yandex.ru>
2017-09-24 17:07:50 +0300

    Первая домашка

--- 01/tasks.js
+++ 01/tasks.js
@@ -5,7 +5,31 @@
  * '1 и 6.45, -2, но 8, а затем 15, то есть 2.7 и -1028' => { min: -1028, max: 15 }
  */
 function getMinMax(string) {
+  const result = {
+    min: Infinity,
+    max: -Infinity
+  };
 
+  for (let i = 0; i < string.length;) {
+    let number = NaN;
+
+    // Пропускаем несколько первых символов, пока не получим число.
+    for (; isNaN(number) && (i < string.length); ++i) {
+      number = parseFloat(string.slice(i));
+    }
+
+    if (!isNaN(number)) {
+      result.min = Math.min(number, result.min);
+      result.max = Math.max(number, result.max);
+
+      // Получили число, но все его суффиксы тоже числа. Их нужно пропустить, так что ждем, пока не получим не число
+      for (; !isNaN(number) && (i < string.length) && (string[i] !== ' '); ++i) {
+        number = parseFloat(string.slice(i));
+      }
+    }
+  }
+
+  return result;
 }
 
 /* ============================================= */
@@ -16,7 +40,14 @@ function getMinMax(string) {
  * @return {number} число под номером х
  */
 function fibonacciSimple(x) {
-  return x;
+  if (x === 0 || x === 1) {
+    return x;
+  }
+  if (x > 1) {
+    return fibonacciSimple(x - 1) + fibonacciSimple(x - 2);
+  }
+
+  return undefined;
 }
 
 /* ============================================= */
@@ -28,7 +59,27 @@ function fibonacciSimple(x) {
  * @return {number} число под номером х
  */
 function fibonacciWithCache(x) {
-  return x;
+  const cache = {
+    0: 0,
+    1: 1
+  };
+
+  function findFibonacciNumber(n) {
+    const key = String(n);
+
+    if (cache[key] !== undefined) {
+      return cache[key];
+    }
+
+    if (n > 1) {
+      cache[key] = findFibonacciNumber(n - 1) + findFibonacciNumber(n - 2);
+      return cache[key];
+    }
+
+    return undefined;
+  }
+
+  return findFibonacciNumber(x);
 }
 
 /* ============================================= */
@@ -49,7 +100,25 @@ function fibonacciWithCache(x) {
  * @return {string}
  */
 function printNumbers(max, cols) {
+  let result = '';
+  const eps = 0.001;
+  const rows = Math.ceil((max + 1) / cols - eps);
 
+  for (let i = 0; i < rows; ++i) {
+    for (let j = 0; i + j * rows <= max; ++j) {
+      const number = i + j * rows;
+
+      result += (number < 10) ? ' ' : '';
+      result += (j > 0) ? ' ' : '';
+      result += String(number);
+    }
+
+    if (i < rows - 1) {
+      result += '\n';
+    }
+  }
+
+  return result;
 }
 
 /* ============================================= */
@@ -60,7 +129,25 @@ function printNumbers(max, cols) {
  * @return {string}
  */
 function rle(input) {
+  let result = '';
+  let last = '';
+  let counter = 1;
 
+  for (let i = 0; i < input.length; ++i) {
+    if (input[i] !== last) {
+      result += last;
+      result += (counter === 1) ? '' : String(counter);
+      counter = 0;
+    }
+
+    last = input[i];
+    counter += 1;
+  }
+
+  result += last;
+  result += (counter === 1) ? '' : String(counter);
+
+  return result;
 }
 
 module.exports = {
