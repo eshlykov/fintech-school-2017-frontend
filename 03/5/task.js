c806694346fa0f6c04390b0de83999d72f8a78f4
Evgeny Shlykov <e.a.shlykov@yandex.ru>
2017-10-11 23:39:31 +0300

    Третья домашка

--- 03/5/task.js
+++ 03/5/task.js
@@ -6,3 +6,32 @@
  * 0..X => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
  * Подсказка - необходимо использовать Proxy - объекты
  * */
+
+function fromRoman(str) {
+  const decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  const roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+  let result = 0;
+  let rest = str;
+
+  for (let i = 0; i < decimal.length; ++i) {
+    while (rest.indexOf(roman[i]) === 0) {
+      result += decimal[i];
+      rest = rest.replace(roman[i], '');
+    }
+  }
+  if (rest === '') {
+    return result;
+  }
+  return undefined;
+}
+
+Object.setPrototypeOf(Number.prototype, new Proxy(Object.getPrototypeOf(Number.prototype), {
+  get: (target, property) => {
+    const length = fromRoman(property);
+
+    if (isFinite(length)) {
+      return Array.from(new Array(length), (value, index) => index);
+    }
+    return undefined;
+  }
+}));
